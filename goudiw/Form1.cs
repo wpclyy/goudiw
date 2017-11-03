fd080aa96d1da09b76104553c420576c88d1f072
wpclyy <wangpengchengwc@126.com>
2017-11-03 10:21:37 +0800

    start

--- goudiw/Form1.cs
+++ goudiw/Form1.cs
@@ -77,7 +77,7 @@ namespace goudiw
 
                 ListView.SelectedListViewItemCollection slic = listView1.SelectedItems;
 
-                //规格值sdsdssd
+                //规格值
                 //Dictionary<string, Dictionary<string, string>> spec = new Dictionary<string, Dictionary<string, string>>();
                 foreach (ListViewItem li in slic)
                 {
